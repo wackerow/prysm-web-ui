357683f1460de61a4c4ebd7ee6305545dfabc095
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-10-16 14:31:06 -0700

    initial onboarding e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,44 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display prysm logo', () => {
-    page.navigateTo();
-    expect(page.getLogoUrl()).toMatch(/.*\/assets\/images\/eth\.svg$/);
 
+  it('should allow a user to create new HD wallet with mnemonic (simple)', () => {
+    page.navigateTo();
+    expect(page.getOnboardingButton().getText()).toEqual('Create a Wallet');
+    page.getOnboardingButton().click();
+    expect(page.getOnboardingHeader()).toEqual('Create a Wallet');
+    page.getHDWalletDiv().click();
+    page.getActiveCardButton().click();
+    const mnemonic = page.getMnemonicPhrase();
+    expect(mnemonic).toMatch(/([a-z]+ ){23}[a-z]+/);
+    page.getContinueButton().click();
+    expect(page.getTextArea().getAttribute('formcontrolname')).toEqual('mnemonic');
+    page.getTextArea().click();
+    page.getTextArea().sendKeys(mnemonic);
+    page.getContinueButton().click();
+    expect(page.getInput(0).getAttribute('formcontrolname')).toEqual('walletDir');
+    page.getContinueButton().click();
+    expect(page.getInput(0).getAttribute('formcontrolname')).toEqual('numAccounts');
+    page.getInput(0).sendKeys(1);
+    page.getContinueButton().click();
+    const invalidPassword = 'invalid';
+    const validPassword = 'Valid$2020';
+    const mismatchPassword = 'Valid$20';
+    expect(page.getInput(0).getAttribute('formcontrolname')).toEqual('password');
+    expect(page.getInput(1).getAttribute('formcontrolname')).toEqual('passwordConfirmation');
+    page.getInput(0).sendKeys(invalidPassword);
+    page.getInput(1).sendKeys(invalidPassword);
+    expect(page.getContinueButton().getAttribute('disabled')).toEqual('true');
+    page.getInput(0).clear();
+    page.getInput(1).clear();
+    page.getInput(0).sendKeys(validPassword);
+    page.getInput(1).sendKeys(mismatchPassword);
+    expect(page.getContinueButton().getAttribute('disabled')).toEqual('true');
+    page.getInput(1).clear();
+    page.getInput(1).sendKeys(validPassword);
+    expect(page.getContinueButton().getAttribute('ng-reflect-disabled')).toEqual('false');
+    page.getContinueButton().click();
+    // TODO: Confirm results
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,72 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getLogoUrl(): Promise<string> {
-    return element(by.css('app-root app-login img')).getAttribute('src') as Promise<string>;
+  /**
+   * /login
+   */
+  getOnboardingButton(): ElementFinder {
+    return element(by.css('[routerlink="/onboarding"]'));
+  }
+
+  /**
+   * /onboarding
+   */
+  getOnboardingHeader(): Promise<string> {
+    return element(by.css('app-choose-wallet-kind .text-white.text-3xl')).getText() as Promise<string>;
+  }
+
+  getHDWalletDiv(): ElementFinder {
+    return element(by.className('onboarding-grid flex-wrap flex justify-center items-center my-auto')).all(by.tagName('div')).get(0);
+  }
+
+  getImportedWalletDiv(): ElementFinder {
+    return element(by.cssContainingText('div', 'Imported Wallet'));
+  }
+
+  getRemoteWalletDiv(): ElementFinder {
+    return element(by.cssContainingText('div', 'Remote Wallet'));
+  }
+
+  getActiveCardButton(): ElementFinder {
+    return element(by.css('app-choose-wallet-kind .onboarding-card.active .wallet-action button'));
+  }
+
+  /**
+   * /onboarding - HD Wallet Setup wizard
+   */
+  getMnemonicPhrase(): Promise<string> {
+    return element(by.tagName('app-generate-mnemonic')).all(by.tagName('div')).get(1).getText() as Promise<string>;
+  }
+
+  getPreviousButton(): ElementFinder {
+    return element(
+      by.css('.mat-horizontal-content-container [aria-expanded="true"]')
+      ).all(by.tagName('button')).get(0);
+  }
+
+  getContinueButton(): ElementFinder {
+    return element(
+      by.css('.mat-horizontal-content-container [aria-expanded="true"]')
+      ).all(by.tagName('button')).get(1);
+  }
+
+  getTextArea(): ElementFinder {
+    return element(
+      by.css('.mat-horizontal-content-container [aria-expanded="true"]')
+      ).all(by.tagName('textarea')).get(0);
+  }
+
+  getInput(i: number): ElementFinder {
+    return element(
+      by.css('.mat-horizontal-content-container [aria-expanded="true"]')
+      ).all(by.tagName('input')).get(i);
+  }
+
+  getWizardComponent(): ElementFinder {
+    return element(by.byTag('app-hd-wallet-wizard'));
   }
 }

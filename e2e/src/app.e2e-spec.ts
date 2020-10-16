9e3d2d939fd9cf575d43bad28a7f3764fb34ae23
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-10-15 18:28:00 -0700

    initial e2e commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,10 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display prysm logo', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('prysm-web-ui app is running!');
+    expect(page.getLogoUrl()).toMatch(/.*\/assets\/images\/eth\.svg$/);
+
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getLogoUrl(): Promise<string> {
+    return element(by.css('app-root app-login img')).getAttribute('src') as Promise<string>;
   }
 }
